b2d488faf1a8bf5eb8682732290237c43b99eba9
Marco <33257157+mcarfiz@users.noreply.github.com>
2021-11-18 16:06:35 +0100

    Minor fixes

--- server.js
+++ server.js
@@ -1,33 +1,5 @@
-//import express from "express";
-/*import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import path from 'path';
-import pkg from 'electron';
-const { app, BrowserWindow } = pkg;*/
-
 const { app, BrowserWindow } = require("electron");
 const path = require('path');
-
-//const { fileURLToPath } = require('url');
-//const { dirname } = require('path');
-
-//const __filename = fileURLToPath(import.meta.url);
-//const __dirname = dirname(__filename);
-
-/*const app = express();
-const port = 3000;
-
-// set /public directory to fetch files
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.listen(port);
-console.log('Listening on port ' + port);
-
-// render index
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html')
-})*/
-
 
 // electron stuff
 function createWindow () {
@@ -41,7 +13,7 @@ function createWindow () {
   })
 
     win.menuBarVisible = false
-    win.loadFile(path.join(__dirname, 'index.html'))
+    win.loadFile(path.join(__dirname, '/public/index.html'))
 }
 
 app.whenReady().then(() => {
