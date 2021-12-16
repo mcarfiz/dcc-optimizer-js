9e10d7d9ff80064ced91fef1047580d7efba6a11
Marco <33257157+mcarfiz@users.noreply.github.com>
2021-12-16 10:38:09 +0100

    General improvement
    Home is now showing dynamically
    Better adv faq button
    Add comments
    Formatting

--- public/core.js
+++ public/core.js
@@ -21,12 +21,14 @@ var show_faq = false;
 var tab;
 var lang = ita;
 
+// on load: load localization and set up qrscanner engine
 $(document).ready(function () {
     tab = "home";
     load_text();
     qrEngine = QrScanner.createQrEngine();
 });
 
+// listener for change on file selector, when a new qr is inserted try to decode it
 fileSelector.addEventListener('change', event => {
     var file = fileSelector.files[0];
     if (!file) return;
@@ -35,33 +37,34 @@ fileSelector.addEventListener('change', event => {
     QrScanner.scanImage(file, null, qrEngine)
         .then(result => {
             text = result;
-            optimize(/*fileQrResult, */text)
+            optimize(text)
         })
         .catch(e => error(e || 'No QR code found.'));
 });
 
+// when a radio is changed re-encode the qr (if it's there)
 radios.addEventListener('change', event => {
     resetPage();
     if (!text) return;
     // scan qr from image
-    optimize(/*fileQrResult, */text);
+    optimize(text);
 });
 
-fileSelector.addEventListener("click", event =>{
+fileSelector.addEventListener("click", event => {
     revertScan();
 });
 
 // camera button listener to activate the camera scanner
 cameraBtn.addEventListener("click", function (element) {
-    if (cameraBtn.className == "btn btn-success"){
+    if (cameraBtn.className == "btn btn-success") {
         html5QrcodeScanner.start({ facingMode: "environment" }, config, onScanSuccess)
-        .then(success => {
-            cameraBtn.className = "btn btn-danger";
-            cameraBtn.value = lang["home"]["stop-scanner"];
-        })
-        .catch(err => {
-            error("No camera was found.");
-        });
+            .then(success => {
+                cameraBtn.className = "btn btn-danger";
+                cameraBtn.value = lang["home"]["stop-scanner"];
+            })
+            .catch(err => {
+                error("No camera was found.");
+            });
     }
     else
         revertScan();
@@ -75,29 +78,27 @@ async function onScanSuccess(decodedText) {
     optimize(text);
 }
 
-function revertScan(){
+// stop camera scanning
+function revertScan() {
     cameraBtn.className = "btn btn-success";
     cameraBtn.value = lang["home"]["qrcamera-btn"]
-    try{html5QrcodeScanner.stop();}
-    catch(error){}
+    try { html5QrcodeScanner.stop(); }
+    catch (error) { }
 }
 
 // optimize function called if the file scan was ok
 async function optimize(/*label,*/ result) {
     var qrString = String(result);
-    // decode of cose content into dcc constant
+    // decode of cose content into dcc variable
     dcc = await DCC.fromRaw(qrString);
     if (dcc) {
-        // remove health certificate version
+        // remove health certificate version, if italian case
         if (document.getElementById('radio-it').checked)
             qrString = qrString.replace("HC1:", "");
 
         // generate new qr code
         // documentation: https://github.com/kozakdenys/qr-code-styling
-        // can be improved to make options be selected by user
         qrCode = new QRCodeStyling({
-            /*width: 512,
-            height: 512,*/
             type: "jpeg",
             data: qrString,
             dotsOptions: {
@@ -116,7 +117,6 @@ async function optimize(/*label,*/ result) {
                 errorCorrectionLevel: 'L'
             }
         });
-
         // show success message
         resMsg.className = "alert alert-success";
         resMsg.innerHTML = lang["home"]["success-msg"]
@@ -124,10 +124,9 @@ async function optimize(/*label,*/ result) {
         qrCode.append(qrCanvas);
         downloadButton.style.display = "table";
     }
-    else {
+    else
         // show error message
         error("Internal problems reading the QR code occurred.");
-    }
 }
 
 // on-click listener for the download button
@@ -136,42 +135,58 @@ downloadButton.addEventListener('click', function () {
     qrCode.download({ name: "QR-Optimized", extension: "jpeg" });
 }, false);
 
+document.getElementById("home-side-btn").addEventListener('click', function () {
+    document.getElementById("close-btn").click();
+    if (tab === "faq") {
+        document.getElementById("faq").click();
+        document.getElementById("main-container").style.display = "table";
+        document.getElementById("faq-card").style.display = "flex";
+    }
+}, false);
+
 // faq sidenav click
-document.getElementById("faq").addEventListener("click", function(){ 
-        tab = "faq";
-        load_text(lang);
-        document.getElementById("close-btn").click();
-        document.getElementById("main-container").style.display = "none";
-        document.getElementById("faq-card").style.display= "none";
-        document.getElementById("nav-title").innerHTML = lang["faq"]["nav-title"];
-        revertScan();
+document.getElementById("faq").addEventListener("click", function () {
+    tab = "faq";
+    // close sidenav and show faq
+    document.getElementById("close-btn").click();
+    document.getElementById("main-container").style.display = "none";
+    document.getElementById("faq-card").style.display = "none";
+    document.getElementById("nav-title").innerHTML = lang["faq"]["nav-title"];
+    // stop camera scanning if active
+    revertScan();
 }, false);
 
 // advanced faq toggle
-$("#advanced-faq-btn").click(function(){
+$("#advanced-faq-btn").click(function () {
     $("#advanced-faq-1").toggle("slow");
     $("#advanced-faq-2").toggle("slow");
-    if (show_faq){
+    // is it showing advanced faqs?
+    if (show_faq) {
         $("#advanced-faq-btn").text(lang["faq"]["adv-btn-show"]);
+        $("#advanced-faq-btn").attr("class", "btn btn-success");
         show_faq = false;
     }
-    else{
+    else {
         $("#advanced-faq-btn").text(lang["faq"]["adv-btn-hide"]);
+        $("#advanced-faq-btn").attr("class", "btn btn-danger");
         show_faq = true;
     }
 });
 
-function load_text(){
+// load text strings from json of the selected language
+function load_text() {
     // home
-    if(tab=="home")
-        $('#nav-title').text(lang["home"]["nav-title"]);    
+    if (tab === "home")
+        $('#nav-title').text(lang["home"]["nav-title"]);
     else
         $('#nav-title').text(lang["faq"]["nav-title"]);
-    $('#main-title').html(lang["home"]["main-title"]);  
-    $('#file-picker').html(lang["home"]["file-selector"]);
+    $('#main-title').html(lang["home"]["main-title"]);
+    $('#file-selector-label').html(lang["home"]["file-selector"]);
     $("#qrcamera-btn").val(lang["home"]["qrcamera-btn"]);
     $("#radio-label-eu").html(lang["home"]["radio-eu"]);
     $("#radio-label-it").html(lang["home"]["radio-it"]);
+    if (resMsg.innerHTML)
+        resMsg.innerHTML = lang["home"]["success-msg"];
     // faq
     $('.question').text(lang["faq"]["question"]);
     $('.answer').text(lang["faq"]["answer"]);
@@ -195,17 +210,24 @@ function load_text(){
     $('#adv-ans3-qr-gen1').html(lang["faq"]["adv-ans3-qr-gen1"]);
     $('#adv-ans3-qr-gen2').html(lang["faq"]["adv-ans3-qr-gen2"]);
     $('#adv-ans4').html(lang["faq"]["adv-ans4"]);
-    $('#advanced-faq-btn').text(lang["faq"]["adv-btn-show"]);
+    if (show_faq)
+        $('#advanced-faq-btn').text(lang["faq"]["adv-btn-hide"]);
+    else
+        $('#advanced-faq-btn').text(lang["faq"]["adv-btn-show"]);
 }
 
-$('#ita').click(function(){
+// italian language on click
+$('#ita').click(function () {
+    // change language to italian
     lang = ita;
     load_text();
     $('#ita').removeAttr("href");
     $('#eng').attr('href', '#');
 });
 
-$('#eng').click(function(){
+// english language on click
+$('#eng').click(function () {
+    // change language to english
     lang = eng;
     load_text();
     $('#eng').removeAttr("href");
